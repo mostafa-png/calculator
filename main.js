e740113b4fed70d248b856b6c81040889dc96a04
mostafa khaled osman <123234339+mostafa-png@users.noreply.github.com>
2024-03-05 09:43:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const userinput = document.getElementById("userinput");
+var expression = ''
+function press(num){
+    expression += num;
+    userinput.value = expression;
+}
+function equal(){
+    userinput.value = eval(expression);
+    expression = "";
+}
+function erase(){
+    expression = '';
+    userinput.value = expression;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
